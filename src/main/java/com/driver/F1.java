2d4531e34d9b74d4a7320e78203501783bf9c799
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-06-10 16:59:43 +0530

    new changes

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,8 +3,10 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name, 1, 2,6, isManual, "Formula 1", 1);;
+
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 2, 6, isManual, "F1", 1);
+
 
     }
 
@@ -20,18 +22,20 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
         int newGear;
-        if (newSpeed <= 50) {
+        if (newSpeed == 0) {
             newGear = 1;
-        } else if (newSpeed <= 100) {
+        } else if (newSpeed >1 && newSpeed<= 50) {
+            newGear = 1;
+        } else if (newSpeed >51 && newSpeed<= 100) {
             newGear = 2;
-        } else if (newSpeed <= 150) {
+        } else if (newSpeed >101 && newSpeed<= 150) {
             newGear = 3;
-        } else if (newSpeed <= 200) {
+        } else if (newSpeed>151 && newSpeed<= 200) {
             newGear = 4;
-        } else if (newSpeed <= 250) {
+        } else if(newSpeed>201 && newSpeed<=250) {
             newGear = 5;
-        } else {
-            newGear = 6;
+        }else{
+            newGear=6;
         }
         changeGear(newGear);
         if (newSpeed > 0) {
