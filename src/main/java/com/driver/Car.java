5996ce02490c7da4a0641cf72d5e79fbc771897a
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-06-10 16:23:21 +0530

    change

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -18,7 +18,7 @@ public class Car extends Vehicle {
         this.isManual=isManual;
         this.type=type;
         this.seats=seats;
-        this.currentGear=0;
+        this.currentGear=1;
 
 
     }
